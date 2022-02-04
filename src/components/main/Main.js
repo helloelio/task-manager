ad9093cb1be1e35964b6dbf84a44b0b41b1ec484
helloelio <fredwantblossom@gmail.com>
2022-02-04 19:52:00 +0300

    Add BreadCrumbs, Background, to pocket content

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,16 +1,34 @@
 import './Main.css';
 import TaskItem from './tasks/taskItem.js';
+import Breadcrumbs from './breadcrumbs/BreadCrumbs';
+import Background from './assets/pocket-bg.jpg';
 
 function Main(props) {
+  const bg = {
+    background: `url('${Background}')`,
+    height: '250px',
+    width: '100%',
+    position: 'absolute',
+    top: '35px',
+    left: '0',
+    backgroundSize: '100%',
+  };
+
   return (
-    <main className='main'>
-      <h2>{props.title}</h2>
+    <main className='main' id='main'>
+      <Breadcrumbs breadcrumbs={props.title} scrollToTop={props.scrollToTop} />
+      <div className='pocket-bg' style={bg}></div>
+      <h2 className='main__title'>{props.title}</h2>
       <ul>
         {props.tasks.map((task) => {
           return <TaskItem id={task.id} task={task.task} key={task.id} />;
         })}
       </ul>
-      <button className='btn btn-main' onClick={() => props.modalOpen('TASK')}>
+      <button
+        className='btn btn-main'
+        onClick={() => props.modalOpen('TASK')}
+        title='Click to add new task in current pocket'
+      >
         +
       </button>
     </main>
