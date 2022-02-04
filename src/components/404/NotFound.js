7a9dd3e3cddb10232d807bbd8942cb84577b53a7
helloelio <fredwantblossom@gmail.com>
2022-02-04 19:49:42 +0300

    Add notFound page Component

---
+++ src/components/404/NotFound.js
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+
+export default function NotFound(props) {
+  const style = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#252525',
+    margin: '0',
+    fontSize: '3rem',
+    textAlign: 'center',
+  };
+  const linkStyle = {
+    textDecoration: 'underline',
+    color: 'rgb(0, 154, 198)',
+  };
+  return (
+    <div style={style}>
+      <h1>This {props.title} is NOT FOUND</h1>
+      <Link to='/tasks/all' style={linkStyle}>
+        Return back
+      </Link>
+    </div>
+  );
+}
