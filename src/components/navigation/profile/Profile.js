f60f14ef17254cd1d6095985d2daa0aab6bafd70
helloelio <fredwantblossom@gmail.com>
2022-02-04 19:50:31 +0300

    refactor

--- src/components/navigation/profile/Profile.js
+++ src/components/navigation/profile/Profile.js
@@ -2,15 +2,19 @@ import './Profile.css';
 import { useState } from 'react';
 
 function Profile() {
-  const [name, setName] = useState('Player');
+  const [name, setName] = useState('helloelio');
   return (
     <div className='profile'>
-      <div className='profile__image'>
-        <span className='avatar'>{name[0].toUpperCase()}</span>
+      <div className='profile-content'>
+        <div className='profile__image'>
+          <span className='avatar'>{name[0].toUpperCase()}</span>
+        </div>
+        <div className='profile__description'>
+          <h3 className='profile__name'>{name}</h3>
+        </div>
       </div>
-      <div className='profile__description'>
-        <h3 className='profile__name'>{name}</h3>
-        <p>example@mail.com</p>
+      <div className='profile__contorls'>
+        <button className='btn btn-profile'> {'<'} </button>
       </div>
     </div>
   );
