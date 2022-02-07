8c93e4d020b68e23069a229d8fd5b8a4d8ea6398
helloelio <fredwantblossom@gmail.com>
2022-02-07 18:56:46 +0300

    create login page

---
+++ src/components/login/LoginPage.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import './LoginPage.css';
+
+export default function LoginPage(props) {
+  const [login, setLogin] = useState('');
+
+  const handlerKeyPresss = (e) => {
+    if (e.key === 'Enter') {
+      props.handlerLogin(e.target.value);
+    }
+  };
+
+  return (
+    <div className='login-page'>
+      <div className='login-input'>
+        <input
+          type='text'
+          id='login'
+          onChange={(e) => setLogin(e.target.value)}
+          onKeyPress={(e) => handlerKeyPresss(e)}
+          //   autoFocus={true}
+        />
+        <label id='login-label' htmlFor='login'>
+          Enter your username:
+        </label>
+      </div>
+      <button
+        className='btn btn-login'
+        onClick={() => props.handlerLogin(login)}
+      >
+        Login
+      </button>
+    </div>
+  );
+}
